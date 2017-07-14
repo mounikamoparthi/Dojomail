d928fae894cb6c045be91e11c682bf6bb02fe447
Mounika <mounika.tunga9@gmail.com>
2017-07-13 22:56:46 -0700

    confirm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  emails = [
+    {
+      Email: "bill@gates.com",
+      Importance: true,
+      Subject: "New Windows",
+      Content: 'Windows XI will launch in year 2100',
+    },
+    {
+      Email: "ada@lovelace.com",
+      Importance: true,
+      Subject: "Programming",
+      Content: "Enchantress of Numbers",
+    },
+    {
+      Email: "john@carmac.com",
+      Importance: false,
+      Subject: "Updated Algo",
+      Content: "New algo for shadow volumes",
+    },
+    {
+      Email: "phil@newel.com",
+      Importance: false,
+      Subject: "HL3!",
+      Content: "Just Kidding...",
+    }
+  ]
 }
